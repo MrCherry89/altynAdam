2a5d08c9e2cd71ce40eabac93d0d86f1e803f5b0
MrCherry89 <abgarnati@yandex.ru>
2024-05-10 16:38:19 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -126,6 +126,18 @@ $(document).ready(function () {
 
   AOS.init();
 
+  $(".popup-gallery-map").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+  });
+
   $(".popup-gallery").magnificPopup({
     delegate: "a",
     type: "image",
